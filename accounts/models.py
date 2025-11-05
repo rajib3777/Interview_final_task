6369cb09ead813a047c83fcbaf6180e0082204cf
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-05 21:07:50 +0600

    Update models.py
    Space_reduction_01

--- accounts/models.py
+++ accounts/models.py
@@ -10,13 +10,9 @@ class UserManager(BaseUserManager):
         
         if not email:
             raise ValueError("Email address is required")
-        
         email = self.normalize_email(email)
-        
         user = self.model(email=email, full_name=full_name, **extra_fields)
-        
         user.set_password(password)
-        
         user.save(using=self._db)
         
         return user
@@ -24,19 +20,13 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, full_name=None, password=None, **extra_fields):
         
         extra_fields.setdefault("is_staff", True)
-        
         extra_fields.setdefault("is_superuser", True)
-        
         extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
-            
             raise ValueError("Superuser must have is_staff=True.")
-        
         if extra_fields.get("is_superuser") is not True:
-            
             raise ValueError("Superuser must have is_superuser=True.")
-
         return self.create_user(email, full_name, password, **extra_fields)
 
 
@@ -57,27 +47,18 @@ class User(AbstractUser):
         )
         
         user_type = models.CharField(max_length=10, choices=ROLE_CHOICES, default='USER')
-        
         phone = models.CharField(max_length=20, blank=True, null=True)
-        
         full_name = models.CharField(max_length=255, blank=True, null=True)
-        
         address = models.CharField(max_length=255, blank=True, null=True)
-        
         image = models.ImageField(upload_to=user_directory_path, null=True, blank=True)
-        
         email = models.EmailField(unique=True)
 
         is_active = models.BooleanField(default=False)
-        
-        
         objects = UserManager()
         
         
         username = models.CharField(max_length=150)
-        
         USERNAME_FIELD = 'email'
-        
         REQUIRED_FIELDS = ['username','full_name']
         
     
