eea70dadb95c5556578524dfba875f4f467be12a
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-05 21:15:56 +0600

    Update urls.py
    Space_reduction_04

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,13 +10,10 @@ from .views import (
 urlpatterns = [
 
     path('register/', RegisterView.as_view(), name='register'),
-
-
+    
     path('login/', LoginView.as_view(), name='login'),
 
-
     path('logout/', LogoutView.as_view(), name='logout'),
-
 
     path('profile/', ProfileView.as_view(), name='profile'),
 
