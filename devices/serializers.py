756e02f159c2371c2d4484d82c4e452d816b326f
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-05 21:18:00 +0600

    Update serializers.py
    Space_reduction_07

--- devices/serializers.py
+++ devices/serializers.py
@@ -7,9 +7,6 @@ from .models import Device
 class DeviceSerializer(serializers.ModelSerializer):
 
     class Meta:
-
         model = Device
-
         fields = ['id', 'device_name', 'device_type', 'os_version', 'created_at']
-        
         read_only_fields = ['id', 'created_at']
